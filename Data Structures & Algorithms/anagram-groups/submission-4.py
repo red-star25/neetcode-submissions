130242215c93863aadf12d54fc6495fa9892197e
Dhruv Nakum <54853762+red-star25@users.noreply.github.com>
2026-07-03 23:21:57 -0700

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        chArr = [0] * 26
+        hm = collections.defaultdict(List)
+
+        for s in strs:
+            chArr = [0] * 26
+            for ch in s:
+                chArr[ord(ch) - 97] += 1
+            
+            if tuple(chArr) not in hm:
+                hm[tuple(chArr)] = [s]
+            else:
+                hm[tuple(chArr)].append(s)
+
+        return list(hm.values())
