9711a2a1c694bdae42bbb35339b515fcf01f930b
Dhruv Nakum <54853762+red-star25@users.noreply.github.com>
2026-07-22 17:23:41 -0700

    Add: is-anagram - submission-13

---
+++ Data Structures & Algorithms/is-anagram/submission-13.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+            
+        hm = collections.defaultdict(int)
+        for ch in s:
+            hm[ch] += 1
+        
+        for ch in t:
+            if ch in hm:
+                hm[ch] -= 1
+                if hm[ch] == 0:
+                    del hm[ch]
+        
+        return len(hm) == 0
